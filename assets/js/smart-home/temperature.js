5e70ea16f8b6830b2dcf87b5e4ea4cf0ffa5f8e4
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-01-29 22:02:31 +0000

    Fix all the JS

---
+++ assets/js/smart-home/temperature.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const SUFFIX_C = '°c';
+const SUFFIX_F = '°f';
+
+export default class Temperature {
+    #degreesC;
+
+    /**
+     * @param {Number} degreesC
+     */
+    constructor(degreesC) {
+        this.#degreesC = degreesC;
+    }
+
+    /**
+     * Returns a string like "21°c".
+     *
+     * @param {Number} decimalPlaces
+     *
+     * @returns {String}
+     */
+    formatC(decimalPlaces = 1) {
+        return this.#degreesC.toFixed(decimalPlaces)+SUFFIX_C;
+    };
+
+    /**
+     * Returns a string like "69.8°f".
+     *
+     * @param {Number} decimalPlaces
+     *
+     * @returns {String}
+     */
+    formatF(decimalPlaces = 1) {
+        return this.#concertToF().toFixed(decimalPlaces)+SUFFIX_F;
+    };
+
+    /**
+     * Returns the fahrenheit equivalent of celsius.
+     *
+     * @returns {Number}
+     */
+    #concertToF() {
+        return (this.#degreesC * 1.8) + 32;
+    };
+}
---
+++ assets/js/smart-home/weather-fetch-wrapper.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import FetchWrapper from './fetch-wrapper.js';
+import WeatherTable from './weather-table.js';
+
+const URL_PROD = 'https://christianbrown.global.ssl.fastly.net/get-met-office-temps';
+const URL_DEV = 'http://localhost:8080/';
+const SELECTOR = '#met-weather-table';
+
+export default class WeatherFetchWrapper extends FetchWrapper {
+    /**
+     * @param {Boolean} dev
+     */
+    constructor(dev = false) {
+        super(dev ? URL_DEV : URL_PROD, new WeatherTable(document.querySelector(SELECTOR)));
+    }
+}
