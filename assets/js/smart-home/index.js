59c6b54875cc83330956b49a64b4f4cc9e7f47ea
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-01-29 22:02:31 +0000

    Split smart-home into JS modules and load scripts as ES modules

---
+++ assets/js/smart-home/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import Time from './time.js';
+import SmartHomeFetchWrapper from './smart-home-fetch-wrapper.js';
+import WeatherFetchWrapper from './weather-fetch-wrapper.js';
+
+function updateTime() {
+    const timeSpan = document.querySelector('#time');
+    timeSpan.textContent = (new Time()).formatHour();
+}
+
+window.onload = function() {
+    const metWeatherFetchWrapper = new WeatherFetchWrapper();
+    const smartHomeFetchWrapper = new SmartHomeFetchWrapper();
+
+    metWeatherFetchWrapper.update();
+    smartHomeFetchWrapper.update();
+    updateTime();
+
+    setInterval(
+        function() {
+            updateTime();
+            smartHomeFetchWrapper.update();
+        },
+        60 * 1000
+    );
+
+    setInterval(metWeatherFetchWrapper.update,5 * 60 * 1000);
+}
---
+++ assets/js/smart-home/time.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const DEFAULT_TIMEZONE = 'Europe/London';
+const DEFAULT_LOCALE = 'en-GB'
+const MS_PER_SEC = 1000;
+const MS_PER_MINUTE = 60;
+const MS_PER_HOUR = MS_PER_MINUTE * 60;
+const MS_PER_DAY = MS_PER_HOUR * 24;
+
+export default class Time {
+    #timestamp;
+    #timezone;
+    #locale;
+
+    /**
+     * @param {Number} timestamp
+     * @param {String} timezone
+     * @param {String} locale
+     */
+    constructor(timestamp = Date.now(), timezone = DEFAULT_TIMEZONE, locale = DEFAULT_LOCALE) {
+        this.#timestamp = timestamp;
+        this.#timezone = timezone;
+        this.#locale = locale;
+    }
+
+    /**
+     * Returns a string like "21 min(s) ago".
+     *
+     * @returns {String}
+     */
+    formatTimeAgo() {
+        const elapsed = Math.floor(Date.now()/MS_PER_SEC) - this.#timestamp;
+        let str;
+        if (elapsed < MS_PER_MINUTE) {
+            str = Math.floor(elapsed) + ' sec(s) ago';
+        } else if (elapsed < MS_PER_HOUR) {
+            str = Math.floor(elapsed/MS_PER_MINUTE) + ' min(s) ago';
+        } else if (elapsed < MS_PER_DAY) {
+            str = Math.floor(elapsed/MS_PER_HOUR ) + ' hr(s) ago';
+        } else {
+            str = Math.floor(elapsed/MS_PER_DAY) + ' day(s) ago';
+        }
+
+        return str;
+    };
+
+    /**
+     * Returns a string like "19:53 (7:53 pm)".
+     *
+     * @returns {String}
+     */
+    formatHour() {
+        return this.#formatHour24()+' ('+this.#formatHour12()+')';
+    }
+
+    /**
+     * Returns a string like "19:53".
+     *
+     * @returns {String}
+     */
+    #formatHour24() {
+        const formatConfig = {
+            'timeZone': this.#timezone,
+            'hourCycle': 'h23',
+            'hour': '2-digit',
+            'minute': '2-digit'
+        };
+        return this.#format(formatConfig);
+    }
+
+    /**
+     * Returns a string like "7:53 pm".
+     *
+     * @returns {String}
+     */
+    #formatHour12() {
+        const formatConfig = {
+            'timeZone': this.#timezone,
+            'hour': 'numeric',
+            'minute': '2-digit',
+            'hour12': true,
+        };
+        return this.#format(formatConfig);
+    }
+
+    /**
+     * Returns a formatted time string.
+     *
+     * @param {DateTimeFormatOptions|Object} formatConfig
+     *
+     * @returns {string}
+     */
+    #format(formatConfig) {
+        const format = new Intl.DateTimeFormat(this.#locale , formatConfig);
+        return format.format(this.#timestamp);
+    }
+}
