eb9a8c4be0bfaea8ed8b7edc76db6c5a96f4806e
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-01-30 00:32:04 +0000

    Add ASCII banner and global.js, rework CV experience markdown logic

---
+++ assets/js/global.js
@@ -0,0 +1,12 @@
+'use strict';
+
+window.addEventListener('load',
+    function() {
+        console.info('\u001b[1;32m                                                                                                                        _');
+        console.info('\u001b[1;32m _|_ |_   _   _  _    _. ._ _   ._   _ _|_  _|_ |_   _    _| ._ _  o  _|  _      _ /    ._ _   |  _   _  |  o ._   _   _|_ _  ._');
+        console.info('\u001b[1;32m  |_ | | (/_ _> (/_  (_| | (/_  | | (_) |_   |_ | | (/_  (_| | (_) | (_| _>  \\/ (_) |_| | (/_  | (_) (_) |< | | | (_|   | (_) |');
+        console.info('\u001b[1;32m                                                                             /                                     _|');
+        console.info('');
+        console.info('\u001b[1;35mbut maybe you\'ve looking for an engineering manager? you\'ve come to right place!');
+    }
+);
