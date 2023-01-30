f2eb361775be8bb2e8518963b06c101e701d2891
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-01-30 00:33:13 +0000

    Fix typo in engineering manager banner text

--- assets/js/global.js
+++ assets/js/global.js
@@ -7,6 +7,6 @@ window.addEventListener('load',
         console.info('\u001b[1;32m  |_ | | (/_ _> (/_  (_| | (/_  | | (_) |_   |_ | | (/_  (_| | (_) | (_| _>  \\/ (_) |_| | (/_  | (_) (_) |< | | | (_|   | (_) |');
         console.info('\u001b[1;32m                                                                             /                                     _|');
         console.info('');
-        console.info('\u001b[1;35mbut maybe you\'ve looking for an engineering manager? you\'ve come to right place!');
+        console.info('\u001b[1;35mbut maybe you\'re looking for an engineering manager? you\'ve come to right place!');
     }
 );
