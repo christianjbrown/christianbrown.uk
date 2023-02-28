fad3c0ca3723fc0b292b85498d300779a5f7d042
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-02-28 23:11:15 +0000

    Add cookie-consent gating for Google Analytics

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,14 +1,54 @@
 'use strict';
 
+const CONSENT_KEY = 'cookie-consent';
+
+const COUNTRIES_REQUIRING_CONSENT = [ 'AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'EU', 'FI', 'FR', 'GB', 'GR', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK'];
+
+import Country from './country.js';
+
 export default class Cookie
 {
-    needs
+    /**
+     * @returns {boolean}
+     */
+    static needsConsent() {
+        const countryCodes = Country.getCountryCodesFromTimezone();
+        const needsConsent = countryCodes.length && countryCodes.filter(value => COUNTRIES_REQUIRING_CONSENT.includes(value)).length;
+
+        return needsConsent;
+    }
+
+    /**
+     * @returns {boolean|null}
+     */
+    static getConsent() {
+        const cookie = Cookie.get(CONSENT_KEY);
+        let value = null;
+        if (cookie === '1') {
+            value = true
+        } else if (cookie === '0') {
+            value = false;
+        }
+
+        return value;
+    }
+
+    /**
+     * @param {boolean} accept
+     *
+     * @returns {boolean}
+     */
+    static setConsent(accept) {
+        Cookie.set(CONSENT_KEY, accept ? '1' : '0');
+    }
+
+
     /**
      * @param {string} name
      *
      * @returns {string}
      */
-    get(name) {
+    static get(name) {
         let c = document.cookie.match(`(?:(?:^|.*; *)${name} *= *([^;]*).*$)|^.*$`)[1]
         if (c) return decodeURIComponent(c)
     }
@@ -18,7 +58,7 @@ export default class Cookie
      * @param {string} value
      * @param {Object} opts
      */
-    set(name, value, opts = []) {
+    static set(name, value, opts = []) {
         // opts['max-age'] = days * 60 * 60 * 24;
         opts = Object.entries(opts).reduce(
             (accumulatedStr, [k, v]) => `${accumulatedStr}; ${k}=${v}`, ''
@@ -30,7 +70,7 @@ export default class Cookie
      * @param {string} name
      * @param {Array}  opts
      */
-    delete(name, opts) {
+    static delete(name, opts) {
         this.set(name, '', {'max-age': -1, ...opts});
     }
 }
--- assets/js/global.js
+++ assets/js/global.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import Cookie from './cookie.js';
+
 const DROIDS = `
                                                                                                                         _
 _|_ |_   _   _  _    _. ._ _   ._   _ _|_  _|_ |_   _    _| ._ _  o  _|  _      _     /._ _   |  _   _  |  o ._   _   _|_ _  ._
@@ -7,9 +9,46 @@ _|_ |_   _   _  _    _. ._ _   ._   _ _|_  _|_ |_   _    _| ._ _  o  _|  _      
                                                                             /                                     _|
 `;
 
+const cookiesDialog = document.getElementById('cookies');
+document.getElementById('cookies-accept').addEventListener('click',
+    () => {
+        cookiesDialog.style.display = 'none';
+        Cookie.setConsent(true);
+        setOptionalCookies();
+    }
+);
+document.getElementById('cookies-decline').addEventListener('click',
+    () => {
+        cookiesDialog.style.display = 'none';
+        Cookie.setConsent(false);
+    }
+);
+
 window.addEventListener('load',
     () => {
         console.log('%c'+DROIDS, 'color: purple; font-weight: bold;');
         console.log('%c but maybe you\'re looking for an engineering manager? you\'ve come to right place!', 'color: #75923C; font-weight: bold;');
+
+        if (Cookie.needsConsent()) {
+            const consent = Cookie.getConsent();
+            if (consent === null) {
+                cookiesDialog.style.display = 'flex';
+            } else if (consent === true) {
+                setOptionalCookies();
+            }
+        } else {
+            setOptionalCookies();
+        }
     }
 );
+
+function setOptionalCookies() {
+    setGoogleAnalyticsCookies();
+}
+
+function setGoogleAnalyticsCookies() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-9Z9L9Z6QHQ');
+}
