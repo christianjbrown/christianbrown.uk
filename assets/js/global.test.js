e279d6b625d4c73f78d50bd1678ed4e215504b4e
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-03-04 13:13:43 +0000

    Comment out JS tests and refine Jest coverage config

--- assets/js/global.test.js
+++ assets/js/global.test.js
@@ -1,19 +1,19 @@
-'use strict'
-
-test ('global.js',() => {
-        let infos = [], eventListeners = [];
-        console.info = (...args) => {
-                infos.push(args);
-        };
-        window.addEventListener = (...args) => {
-                eventListeners.push(args);
-        };
-        require('./global.js');
-        expect(infos).toHaveLength(0);
-        expect(eventListeners).toHaveLength(1);
-        expect(eventListeners[0][0]).toBe('load');
-        expect(typeof eventListeners[0][1]).toBe('function');
-        eventListeners[0][1]();
-        expect(infos).toHaveLength(6);
-    }
-);
+// 'use strict'
+//
+// test ('global.js',() => {
+//         let infos = [], eventListeners = [];
+//         console.info = (...args) => {
+//                 infos.push(args);
+//         };
+//         window.addEventListener = (...args) => {
+//                 eventListeners.push(args);
+//         };
+//         require('./global.js');
+//         expect(infos).toHaveLength(0);
+//         expect(eventListeners).toHaveLength(1);
+//         expect(eventListeners[0][0]).toBe('load');
+//         expect(typeof eventListeners[0][1]).toBe('function');
+//         eventListeners[0][1]();
+//         expect(infos).toHaveLength(6);
+//     }
+// );
