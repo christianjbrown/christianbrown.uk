85d625c55320e32e1155447e1bce0e972fa4f7d5
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-03-04 16:29:52 +0000

    Extract JS constants into separate .const.js modules

---
+++ assets/js/smart-home/index.const.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export const CLOCK_SPAN_ID = 'clock';
+export const CLOCK_SPAN_SELECTOR = '#'+CLOCK_SPAN_ID;
+export const HOME_TEMP_TABLE_ID = 'home-temperature-table';
+export const HOME_TEMP_TABLE_SELECTOR = '#'+HOME_TEMP_TABLE_ID;
+export const WEATHER_TABLE_ID = 'weather-table';
+export const WEATHER_TABLE_SELECTOR = '#'+WEATHER_TABLE_ID;
