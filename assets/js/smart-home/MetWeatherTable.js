db1c189fce78004bfec229c1b290cb57973dd042
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-11-20 01:29:41 +0000

    Add update-time timestamp spans to smart-home tables

--- assets/js/smart-home/MetWeatherTable.js
+++ assets/js/smart-home/MetWeatherTable.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import UpdatingKeyValuePairTable from './UpdatingKeyValuePairTable.js';
+import Time from './Time.js';
 
 const JSON_CONTRACT = {
     'temp': {'type': 'number', 'keyRequired': true, 'cannotBeEmpty': true},
@@ -14,6 +15,11 @@ export default class MetWeatherTable extends UpdatingKeyValuePairTable {
      * @param {Object} data
      */
     _renderUpdate(data) {
+        if (data['valid_from']) {
+            const timeObj = new Time(data['valid_from'] * 1000);
+            const timeDiff = timeObj.formatTimeAgo();
+            this._updateDateSpan('From '+timeDiff);
+        }
         this._addTempTableRow('Temperature', data['temp'], null, false, true);
         this._addTempTableRow('Temperature feels like', data['temp_feels_like']);
         this._addTableRow('Humidity', data['humidity']+'%');
